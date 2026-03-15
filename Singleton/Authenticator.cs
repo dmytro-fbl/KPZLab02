1ca03e274f98018d5d4755c3da5a1c7107c239dc
dmytro-fbl <dmytrowork06@gmail.com>
2026-03-15 20:35:02 +0200

    Написав Singleton

---
+++ Singleton/Authenticator.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    public sealed class Authenticator
+    {
+        private static Authenticator _instance;
+        private static readonly object _lock = new object();
+        private Authenticator()
+        {
+
+        }
+        public static Authenticator Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    lock (_lock)
+                    {
+                        if (_instance == null)
+                        {
+                            _instance = new Authenticator();    
+                        }
+                    }
+                }
+                return _instance;
+            }
+        }
+
+        public void ShowMessage()
+        {
+            Console.WriteLine("Єдиний екземпляp");
+        }
+        
+    }
+}
---
+++ Singleton/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Singleton
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Authenticator auth1 = Authenticator.Instance;
+            auth1.ShowMessage();
+
+            Authenticator auth2 = Authenticator.Instance;
+
+            if (ReferenceEquals(auth1, auth2))
+            {
+                Console.WriteLine("Перевірка успішна: 2 об'єкти однаковий екземпляр");
+            }
+            else
+            {
+                Console.WriteLine("Помилка: різні екземпляри");
+            }
+
+            Console.WriteLine($"Auth1 Hash: {auth1.GetHashCode()}");
+            Console.WriteLine($"Auth2 Hash: {auth2.GetHashCode()}");
+        }
+    }
+}
