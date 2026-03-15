d35f3257990427c24acae3982a2042eaff7b4132
dmytro-fbl <dmytrowork06@gmail.com>
2026-03-15 21:35:42 +0200

    Зробив Prototype

---
+++ Prototype/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Prototype
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Virus v3 = new Virus(0.1, 1, "альфа", "Грип");
+
+            Virus v2 = new Virus(0.5, 3, "бета", "Грип", 1);
+            v2.Children[0] = v3;
+            Virus v1 = new Virus(1.0, 10, "гама", "Грип", 1);
+            v1.Children[0] = v2;
+
+            Virus cloneOfParent = (Virus)v1.Clone();
+
+            Console.WriteLine($"Оригінальний батько: {v1.Name}");
+            Console.WriteLine($"Клонований батько: {cloneOfParent.Name}");
+
+            v1.Children[0].Children[0].Name = "Супер-Мутант";
+
+            Console.WriteLine("\nПісля мутації оригіналу:");
+            Console.WriteLine($"альфа в оригіналі: {v1.Children[0].Children[0].Name}");
+            Console.WriteLine($"альфа у клоні: {cloneOfParent.Children[0].Children[0].Name}");
+        }
+    }
+}
---
+++ Prototype/Virus.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prototype
+{
+    public class Virus : ICloneable
+    {
+        public double Weight { get; set; }
+        public int Age { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+
+        public Virus[] Children { get; set; }
+
+        public Virus(double weight, int age, string name, string type, int childrenCount = 0)
+        {
+            Weight = weight;
+            Age = age;
+            Name = name;
+            Type = type;
+            Children = new Virus[childrenCount];
+        }
+
+        public object Clone()
+        {
+            Virus clone = new Virus(this.Weight, this.Age, this.Name, this.Type, this.Children.Length);
+
+            for (int i = 0; i < this.Children.Length; i++)
+            {
+                if(this.Children[i] != null)
+                {
+                    clone.Children[i] = (Virus)this.Children[i].Clone();
+                }
+            }
+            return clone;
+        }
+    }
+}
